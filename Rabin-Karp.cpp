39d404dc09b2a871692e4d5d9fcac214728857b9
emorysalaun <78174493+emorysalaun@users.noreply.github.com>
2021-12-04 12:55:25 -0500

    Add files via upload
    Added the option to give arguments directly through command prompt
    Added support for input files
    Added code to find every instance of a patter, not just the first.

--- Rabin-Karp.cpp
+++ Rabin-Karp.cpp
@@ -1,31 +1,48 @@
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 int hash(std::string str);
-int RabinKarp(std::string str, std::string pat);
+std::vector<int> RabinKarp(std::string str, std::string pat);
+std::string openFile(std::string filename);
 
-int main(){
+int main(int argc, char* argv[]){
 
     //The string you want to search for the pattern
-    std::string str = "jqkwehqkjwehqoiadiojwHellokjqwebqweoqweiqwemc";
+    std::string str = "";
     //The pattern you are searching for
-    std::string pat = "emc";
+    std::string pat = "";
 
-    int result = RabinKarp(str,pat);
 
-    if(result != -1){
-        std::cout << pat << " found at position " << result;
+    if(argc < 3){
+        std::cout << "Usage: ./Rabin-Karp <string> <pattern> or ./Rabin-Karp file <input filename> <pattern>" << std::endl;
+        return -1;
     }
-    else{
-        std::cout << pat <<" not found.";
+
+    else if(argc == 3){
+        str = argv[1];
+        pat = argv[2];}
+
+    else if(argc == 4){
+        str = openFile(argv[2]);
+        pat = argv[3];
     }
+
+
+    std::vector<int> result = RabinKarp(str,pat);
+
+    for(int i = 0; i < result.size(); i++){
+        std::cout << "\"" << pat << "\" found at position " << result.at(i) << std::endl;
+    }
+
     return 0;
 }
 
 
-int RabinKarp(std::string str, std::string pat){
+std::vector<int> RabinKarp(std::string str, std::string pat){
 
-
+    std::vector<int> finds;
     //Hashes the pattern for comparison
     int h_pat = hash(pat);
 
@@ -40,18 +57,20 @@ int RabinKarp(std::string str, std::string pat){
             //Applies a rolling hash, subtracting the left letter, and adding the right letter
             h_str += (int)str[i+m-1];
             h_str -= (int)str[i-1];}
-        std::cout << "Hash("<< str.substr(i, m)<<")"  << h_str << std::endl;
+
+        //Debug Statement
+        //std::cout << "Hash("<< str.substr(i, m)<<")"  << h_str << std::endl;
 
         //If both hashes are equal, then check for similarity
         if(h_pat == h_str){
             //If all letters are the same, return position.
             if(str.substr(i, m) == pat){
-                return i;
+                finds.push_back(i);
             }
         }
     }
-    //If not found, return -1;
-    return -1;
+
+    return finds;
 }
 
 
@@ -66,4 +85,21 @@ int hash(std::string str){
 
 
     return hash;
+}
+
+std::string openFile(std::string filename){
+    std::ifstream source;
+    source.open(filename);
+
+
+    if(source.is_open()){
+        std::string full;
+        std::string line;
+        while(getline(source, line)){
+                full += line;
+        source.close();
+    }
+    return full;
+}
+return "open file failed";
 }
